3986df578c779b0f13ba7a78782dd486786f3bbe
Dante Souza e Souza <lokinmodar@gmail.com>
2020-09-23 20:30:37 -0300

    Fixing Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,27 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es6: true,
+    node: true,
   },
-  parser: "babel-eslint",
   extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react',
-    'eslint:recommended',
-    'plugin:react/recommended'
+    'airbnb-base',
+    'prettier'
   ],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: 'babel-eslint',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2018,
-    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-    'babel'
-  ],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn',
-      { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off',
-    "react/prefer-stateless-function": 0,
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "react/prop-types": 0,
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-const dotenv = require('dotenv/config');
 
 const app = express();
 
@@ -17,43 +16,39 @@ app.listen(port, () => {
   console.log('We are live on port 4444');
 });
 
-
 app.get('/', (req, res) => {
   res.send('Welcome to my api');
-})
-
-app.post('/api/v1', (req,res) => {
-  var data = req.body;
-
-var smtpTransport = nodemailer.createTransport({
-  service: 'Gmail',
-  port: 465,
-  auth: {
-    user: process.env.GMAIL_username,
-    pass: process.env.GMAIL_pass
-  }
 });
 
-var mailOptions = {
-  from: data.email,
-  to: process.env.EMAIL_address,
-  subject: 'Contato via formulário do site',
-  html: `<p>Contato: ${data.name}</p>
+app.post('/api/v1', (req, res) => {
+  const data = req.body;
+
+  const smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port: 465,
+    auth: {
+      user: process.env.GMAIL_username,
+      pass: process.env.GMAIL_pass,
+    },
+  });
+
+  const mailOptions = {
+    from: data.email,
+    to: process.env.EMAIL_address,
+    subject: 'Contato via formulário do site',
+    html: `<p>Contato: ${data.name}</p>
           <p>Endereço de e-mail: ${data.email}</p>
-          <p>Mensagem: ${data.message}</p>`
-};
+          <p>Mensagem: ${data.message}</p>`,
+  };
 
-smtpTransport.sendMail(mailOptions,
-(error, response) => {
-  if(error) {
-    res.send(error)
-    console.log("erro" + error)
-  }else {
-    res.send({ result: 'Success' })
-    console.log(response.body)
-  }
-  smtpTransport.close();
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      res.send(error);
+      console.log(`erro${error}`);
+    } else {
+      res.send({ result: 'Success' });
+      console.log(response.body);
+    }
+    smtpTransport.close();
+  });
 });
-
-
-})
